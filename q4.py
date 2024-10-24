802224b6e01af7ace679abe7418bfa85a1864e07
mwamali <mtanabrian@gmail.com>
2024-10-24 14:55:22 +0300

    added question 4

---
+++ q4.py
@@ -0,0 +1,17 @@
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+operation = input("Enter an operation (+, -, *, /): ")
+
+if operation == '+':
+    print("Result:", num1 + num2)
+elif operation == '-':
+    print("Result:", num1 - num2)
+elif operation == '*':
+    print("Result:", num1 * num2)
+elif operation == '/':
+    if num2 != 0:
+        print("Result:", num1 / num2)
+    else:
+        print("Error: Division by zero is not allowed.")
+else:
+    print("Invalid operation.")
