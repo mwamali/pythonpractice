641e8b2cd1a5b92429d951660e6892db3742e9ea
mwamali <mtanabrian@gmail.com>
2024-10-24 15:08:22 +0300

    changed the days of the week in q8, starts from Sunday to Saturday

--- q8.py
+++ q8.py
@@ -1,18 +1,18 @@
 day_number = int(input("Enter a number (1-7): "))
 
 if day_number == 1:
-    print("Day: Monday")
-elif day_number == 2:
-    print("Day: Tuesday")
-elif day_number == 3:
-    print("Day: Wednesday")
-elif day_number == 4:
-    print("Day: Thursday")
-elif day_number == 5:
-    print("Day: Friday")
-elif day_number == 6:
-    print("Day: Saturday")
-elif day_number == 7:
     print("Day: Sunday")
+elif day_number == 2:
+    print("Day: Monday")
+elif day_number == 3:
+    print("Day: Tuesday")
+elif day_number == 4:
+    print("Day: Wednesday")
+elif day_number == 5:
+    print("Day: Thursday")
+elif day_number == 6:
+    print("Day: Friday")
+elif day_number == 7:
+    print("Day: Saturday")
 else:
     print("Invalid input.")
